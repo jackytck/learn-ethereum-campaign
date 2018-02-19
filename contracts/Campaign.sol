6cf6ec51801a6343e3e270e97490c4b35193ade4
Jacky Tang <jackytck@gmail.com>
2018-02-20 00:16:29 +0800

    :sparkles: add modifier: restricted

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -13,14 +13,19 @@ contract Campaign {
     uint public minimumContribution;
     address[] public approvers;
 
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
     function Campaign(uint minimum) public {
         manager = msg.sender;
         minimumContribution = minimum;
     }
-    
+
     function contribute() public payable {
         require(msg.value > minimumContribution);
-        
+
         approvers.push(msg.sender);
     }
 }
