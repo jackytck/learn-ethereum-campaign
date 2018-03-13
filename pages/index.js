57736f2f23067ca0d04a5a0d636da4051aa017d6
Jacky Tang <jackytck@gmail.com>
2018-03-14 00:03:00 +0800

    :construction: wip: CampaignIndex page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default () => {
-  return (
-    <h1>This is the new campaign page!!!</h1>
-  )
+import factory from '../ethereum/factory'
+
+class CampaignIndex extends Component {
+  async componentDidMount () {
+    const campaigns = await factory.methods.getDeployedCampaigns().call()
+
+    console.log(campaigns)
+  }
+
+  render () {
+    return (
+      <div>
+        Campaign Index!
+      </div>
+    )
+  }
 }
+
+export default CampaignIndex
