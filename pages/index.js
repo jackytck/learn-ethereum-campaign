7115e793b04a6f585b22f3a79b29827c4aef4988
Jacky Tang <jackytck@gmail.com>
2018-03-17 00:20:37 +0800

    :sparkles: show all open campaigns in semantic-ui

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
+import { Button, Card } from 'semantic-ui-react'
 import React, { Component } from 'react'
 
+import Layout from '../components/layout'
 import factory from '../ethereum/factory'
 
 class CampaignIndex extends Component {
@@ -9,11 +11,30 @@ class CampaignIndex extends Component {
     return { campaigns }
   }
 
+  renderCampaigns () {
+    const items = this.props.campaigns.map(address => ({
+      header: address,
+      description: <a>View Campaign</a>,
+      fluid: true
+    }))
+
+    return <Card.Group items={items} />
+  }
+
   render () {
     return (
-      <div>
-        {this.props.campaigns[0]}
-      </div>
+      <Layout>
+        <div>
+          <link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css'></link>
+          <h3>Open Campaings</h3>
+          {this.renderCampaigns()}
+          <Button
+            content='Create Campaign'
+            icon='add circle'
+            primary
+          />
+        </div>
+      </Layout>
     )
   }
 }
