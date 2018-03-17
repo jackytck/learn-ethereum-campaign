be29611f5f3b72aeb1ac4281c459b1476dd4d31d
Jacky Tang <jackytck@gmail.com>
2018-03-17 12:12:16 +0800

    :lipstick: ui tunning

--- components/header.js
+++ components/header.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default () => {
   return (
-    <Menu>
+    <Menu style={{ marginTop: '10px' }}>
       <Menu.Item>
         CrowdCoin
       </Menu.Item>
--- pages/index.js
+++ pages/index.js
@@ -27,12 +27,13 @@ class CampaignIndex extends Component {
         <div>
           <link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css'></link>
           <h3>Open Campaings</h3>
-          {this.renderCampaigns()}
           <Button
+            floated='right'
             content='Create Campaign'
             icon='add circle'
             primary
           />
+          {this.renderCampaigns()}
         </div>
       </Layout>
     )
