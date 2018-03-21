1cd514468d95efd703b8c52f6ff25456b73076b6
Jacky Tang <jackytck@gmail.com>
2018-03-22 00:02:38 +0800

    :construction: get contract summary in CampaignShow

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react'
 
+import Campaign from '../../ethereum/campaign'
 import Layout from '../../components/layout'
 
 class CampaignShow extends Component {
+  static async getInitialProps (props) {
+    const address = props.query.address
+    const campaign = Campaign(address)
+    const summary = await campaign.methods.getSummary().call()
+
+    return {
+      minimumContribution: summary[0],
+      balance: summary[1],
+      requestsCount: summary[2],
+      approversCount: summary[3],
+      manager: summary[4]
+    }
+  }
+
   render () {
     return (
       <Layout>
